bf4cf8161fa72af408536764329bc2b8f438a172
Yiwen <crazyyiwen2015@gmail.com>
2025-09-22 21:52:57 -0400

    add feature

---
+++ api_interface/state_check_update_api.py
@@ -0,0 +1,72 @@
+from datetime import datetime
+from typing import Dict
+from fastapi import APIRouter, HTTPException, Query
+from langchain_core.messages import HumanMessage
+
+
+from services.llm_build_service import AgentState, app_graph
+
+api_router = APIRouter()
+
+
+#---- Get thread ----
+@api_router.get("/api/get_current_state_by_thread_id")
+def get_current_state_by_thread_id(thread_id: str = Query(..., description="Thread ID to fetch")):
+    try:
+        # Build config with thread_id
+        config = {"configurable": {"thread_id": thread_id}}
+        thread_data_1 = app_graph.get_state(config)
+        return {
+            "message": f"Thread state retrieved for thread_id={thread_id}",
+            "thread_data": thread_data_1,
+            "time_started": "Alarm Security System Agent is running well",
+            "version": "1.0.0"
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@api_router.get("/api/get_chat_history_thread_id")
+def get_chat_history_thread_id(thread_id: str = Query(..., description="Thread ID to fetch")):
+    try:
+        # Build config with thread_id
+        config = {"configurable": {"thread_id": thread_id}}
+        chat_history = list(app_graph.get_state_history(config))
+        return {
+            "message": f"Thread state retrieved for thread_id={thread_id}",
+            "thread_data": chat_history,
+            "time_started": "Alarm Security System Agent is running well",
+            "version": "1.0.0"
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+#---- Get thread ----
+@api_router.get("/api/get_checkpoint_by_checkpoint_id")
+def get_checkpoint_by_checkpoint_id(thread_id: str = Query(..., description="Thread ID to fetch"), checkpoint_id: str = Query(..., description="Checkpoint ID to fetch")):
+    try:
+        # Build config with thread_id
+        config = {"configurable": {"thread_id": thread_id, "checkpoint_id": checkpoint_id}}
+        state_data = app_graph.get_state(config)
+
+        return {
+            "message": f"Thread state retrieved for thread_id={thread_id}, checkpoint_id={checkpoint_id}",
+            "state_data": state_data,
+            "time_started": "Alarm Security System Agent is running well",
+            "version": "1.0.0"
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@api_router.post("/api/update_state_by_checkpoint_id")
+def update_state_by_checkpoint_id(thread_id: str = Query(..., description="Thread ID to fetch")):
+    try:
+        config = {"configurable": {"thread_id": thread_id}}
+        # One pending issue - not able to update result, lacking reducer
+        app_graph.update_state(
+            config,
+            {
+                "messages": [HumanMessage(content="test for update")]
+            }
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
