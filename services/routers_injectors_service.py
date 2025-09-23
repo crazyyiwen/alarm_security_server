9a56e6a08a8d884e3d99ed4f2c8eb61e8c663503
Yiwen <crazyyiwen2015@gmail.com>
2025-09-22 21:56:53 -0400

    add

--- services/routers_injectors_service.py
+++ services/routers_injectors_service.py
@@ -1,5 +1,6 @@
-from api_interface import security_system_api
+from api_interface import security_system_api, state_check_update_api
 
 routers = [
-    security_system_api.api_router
+    security_system_api.api_router,
+    state_check_update_api.api_router
 ]
