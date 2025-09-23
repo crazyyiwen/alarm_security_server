b1e3a220a67ab357149e56a988b3fba5a9113a26
Yiwen <crazyyiwen2015@gmail.com>
2025-09-22 21:54:22 -0400

    add feature

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="Alarm Security System Agent", version="1.0.0")
 # ---- Enable CORS ----
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:5218"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
